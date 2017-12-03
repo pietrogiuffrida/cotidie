44e7f40953f89ef141046a47687e04951eeb8bd5
Pietro Giuffrida <pietro.giuffri@gmail.com>
2017-12-04 00:37:05 +0100

    Increased aplay speed rate

--- config.py
+++ config.py
@@ -6,18 +6,20 @@ weather_key = "5ecc57e0157570e7f2e65e4979bf0ddb"
 bologna = '6541998'
 params = "&lang=it&units=metric"
 baseweatherurl = "http://api.openweathermap.org/data/2.5"
-txt_oggi = "Previsioni: {0}. Temperatura minima {1}, massima {2}"
 
 weather = "{0}/weather?id={1}&appid={2}{3}".format(baseweatherurl, bologna, weather_key, params)
 forecast = "{0}/forecast?id={1}&appid={2}{3}".format(baseweatherurl, bologna, weather_key, params)
 
 pico = "pico2wave -l=it-IT -w={}"
-aplay = "aplay {}"
-#remove = "rm {}"
+aplay = "aplay -r 10000 {}"
 
-meteofile = 'messages/meteo.wav'
+meteofile = 'messages/99_meteo.wav'
 
 reboot = True
 msgs = {
-  'messages/bus.wav': "Hai preso il biglietto dell'autobus?",
+  'messages/01.wav': "Buongiorno!",
+  'messages/02_bus.wav': "Pietro, hai preso il biglietto dell'autobus?",
+  'messages/03_fra.wav': "Francesco, ti sei messo il giubotto?",
+  'messages/04_ali.wav': "Alice, hai preso la testa?",
+  'messages/05_mad.wav': "Ciao Maddi!",
   }
