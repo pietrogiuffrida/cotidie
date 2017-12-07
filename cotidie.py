a3a2d72f216ad901f1db9ab57e86185e50b97201
Pietro Giuffrida <pietro.giuffri@gmail.com>
2017-12-08 00:54:37 +0100

    as a lib

--- cotidie.py
+++ cotidie.py
@@ -10,7 +10,6 @@ from time import sleep
 from config import *
 from lib import *
 
-
 logging.basicConfig(
   format='%(asctime)s\t%(levelname)s\t%(message)s',
   filename="cotidie.log",
@@ -18,63 +17,66 @@ logging.basicConfig(
   #level=logging.DEBUG,
   )
 
-logging.info("*"*20 + " NUOVA ESECUZIONE " + "*"*20)
+
+if __name__ == "__main__":
+
+  logging.info("*"*20 + " NUOVA ESECUZIONE " + "*"*20)
 
 
-# ** * ** * ****  ** * ** * ****  ** * ** * ****  ** * ** * ****
-# Messaggi standard
-# ** * ** * ****  ** * ** * ****  ** * ** * ****  ** * ** * ****
+  # ** * ** * ****  ** * ** * ****  ** * ** * ****  ** * ** * ****
+  # Messaggi standard
+  # ** * ** * ****  ** * ** * ****  ** * ** * ****  ** * ** * ****
 
-if reboot == True:
-  for msg in msgs:
+  if reboot == True:
+    for msg in msgs:
 
-    if os.path.exists(msg):
-      os.remove(msg)
+      if os.path.exists(msg):
+        os.remove(msg)
+
+      # 2 synthetize
+      cmd = pico.format(msg).split()
+      cmd.append(msgs[msg])
+      status = run(cmd, testo="pico")
+      if status != 0:
+        os._exit(1)
+
+
+  # ** * ** * ****  ** * ** * ****  ** * ** * ****  ** * ** * ****
+  # Messaggi meteo
+  # ** * ** * ****  ** * ** * ****  ** * ** * ****  ** * ** * ****
+
+  now = datetime.now()
+
+  if os.path.exists(meteofile):
+    os.remove(meteofile)
+
+  if meteo == True:
+    # 1 forecast
+    status, previsioni_oggi = prevedi(forecast, now, delta=12)
 
     # 2 synthetize
-    cmd = pico.format(msg).split()
-    cmd.append(msgs[msg])
+    cmd = pico.format(meteofile).split()
+    cmd.append(previsioni_oggi)
     status = run(cmd, testo="pico")
     if status != 0:
       os._exit(1)
 
 
-# ** * ** * ****  ** * ** * ****  ** * ** * ****  ** * ** * ****
-# Messaggi meteo
-# ** * ** * ****  ** * ** * ****  ** * ** * ****  ** * ** * ****
+  # ** * ** * ****  ** * ** * ****  ** * ** * ****  ** * ** * ****
+  # Play
+  # ** * ** * ****  ** * ** * ****  ** * ** * ****  ** * ** * ****
 
-now = datetime.now()
+  msgs = list(msgs)
+  msgs.append(meteofile)
 
-if os.path.exists(meteofile):
-  os.remove(meteofile)
-
-if meteo == True:
-  # 1 forecast
-  status, previsioni_oggi = prevedi(forecast, now, delta=12)
-
-  # 2 synthetize
-  cmd = pico.format(meteofile).split()
-  cmd.append(previsioni_oggi)
-  status = run(cmd, testo="pico")
-  if status != 0:
-    os._exit(1)
+  for msg in sorted(msgs):
+    cmd = aplay.format(msg).split()
+    status = run(cmd, testo="aplay")
+    if status != 0:
+      os._exit(1)
+    sleep(.7)
 
 
-# ** * ** * ****  ** * ** * ****  ** * ** * ****  ** * ** * ****
-# Play
-# ** * ** * ****  ** * ** * ****  ** * ** * ****  ** * ** * ****
+  # ** * ** * ****  ** * ** * ****  ** * ** * ****  ** * ** * ****
 
-msgs = list(msgs)
-msgs.append(meteofile)
-
-for msg in sorted(msgs):
-  cmd = aplay.format(msg).split()
-  status = run(cmd, testo="aplay")
-  if status != 0:
-    os._exit(1)
-  sleep(.7)
-
-
-# ** * ** * ****  ** * ** * ****  ** * ** * ****  ** * ** * ****
-
-logging.info("*"*20 + " ESCO NORMALMENTE " + "*"*20)
+  logging.info("*"*20 + " ESCO NORMALMENTE " + "*"*20)
